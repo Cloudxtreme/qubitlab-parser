1f78a41292b8313fbdc11db7d12a7863f8336629
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-09-25 21:57:12 +0200

    Adding parser class and small changes in other files.

---
+++ core/parser/parser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Parser:
+
+    def parse_code(self, code, imported_objects):
+        print code
+        # parse code and add new current to
+        return imported_objects
