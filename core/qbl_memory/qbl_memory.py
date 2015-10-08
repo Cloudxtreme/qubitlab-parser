6805cbe9e08f6722558551d8cc525255fb6ba6af
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-10-08 22:37:58 +0200

    QblMemory - beginning.

--- core/qbl_memory/qbl_memory.py
+++ core/qbl_memory/qbl_memory.py
@@ -4,5 +4,30 @@
 
 class QblMemory:
 
-    def example(self):
+    def __init__(self):
+        self.qbl_objects = {
+            'gates': {},
+            'circuits': {},
+            'states': {}
+        }
+
+        self.qbl_tmb_objects = {
+            'gates': {},
+            'circuits': {},
+            'states': {}
+        }
+
+    def add_new_object(self, object_name, object_type, object_data):
+        pass
+
+    def commit_new_objects(self):
+        pass
+
+    def revert_new_objects(self):
+        pass
+
+    def remove_object(self, object_name):
+        pass
+
+    def remove_all(self):
         pass
