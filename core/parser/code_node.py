4d86690647d6fc0e6ebc382538c0dc3fc932792f
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-10-17 10:14:28 +0200

    Changes in CodeNode settings.

--- core/parser/code_node.py
+++ core/parser/code_node.py
@@ -7,6 +7,7 @@ class CodeNode:
     def __init__(self, qbl_memory):
         self.qbl_memory = qbl_memory
         self.node_stack = []
+        self.current_level = 0
 
         self.nodes_settings = {
             'create_qubit_state': {
@@ -44,15 +45,15 @@ class CodeNode:
                     'add_bit_to_input': True,
                     'add_qubit_to_input': True,
                 },
+                'not_after_node': {
+                    'define_step': True
+                },
                 'min_numb_of_children': 1,
                 'only_one_in_parent_node': True
             },
             'define_step': {
                 'available_children': {
                     'add_item_to_step': True
-                },
-                'not_before_node': {
-                    'define_input': True
                 },
                 'min_numb_of_children': 1
             },
@@ -70,14 +71,14 @@ class CodeNode:
             },
         }
 
-    def process_line(self, line_data, identation_level, line_number):
-        if self.valid_node_settings(line_data, identation_level, line_number):
-            self.update_tmp_qbl_memory(line_data)
+    def process_line(self, line_data, indentation_level, line_number):
+        if self.valid_node_settings(line_data, indentation_level, line_number):
+            self.update_tmp_qbl_memory(line_data, indentation_level)
 
-    def valid_node_settings(self, line_data, identation_level, line_number):
+    def valid_node_settings(self, line_data, level_indentation_diff, line_number):
         return True
 
-    def update_tmp_qbl_memory(self, line_data):
+    def update_tmp_qbl_memory(self, line_data, level_indentation_diff):
         pass
 
     def save_qbl_memory(self):
