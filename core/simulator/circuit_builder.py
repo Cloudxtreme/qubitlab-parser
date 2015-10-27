d5f3ea3cbf2dfd98aaa5ac93d9e31c2323018981
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-10-27 10:33:13 +0100

    First methods for classes Simulator/Simulator and Simulator/CircuitBuilder.

--- core/simulator/circuit_builder.py
+++ core/simulator/circuit_builder.py
@@ -2,7 +2,25 @@
 # -*- coding: utf-8 -*-
 
 
-class CicruitBuilder:
+class CircuitBuilder:
 
     def __init__(self):
-        pass
+        self.qbl_memory = None
+
+    def set_qbl_memory(self, qbl_memory):
+        self.qbl_memory = qbl_memory
+
+    def get_circuit(self, circuit_name):
+        circuit_qbl_data = self.get_circuit_qbl_data(circuit_name)
+        circuit_structure = self.replace_variables_to_structures(circuit_qbl_data)
+        circuit_object = self.get_circuit_object(circuit_structure)
+        return circuit_object
+
+    def get_circuit_qbl_data(self, circuit_name):
+        return {}
+
+    def replace_variables_to_structures(self, circuit_qbl_data):
+        return {}
+
+    def get_circuit_object(self, circuit_structure):
+        return {}
--- core/simulator/simulator.py
+++ core/simulator/simulator.py
@@ -6,3 +6,6 @@ class Simulator:
 
     def __init__(self):
         pass
+
+    def run(self, circuit_name):
+        pass
