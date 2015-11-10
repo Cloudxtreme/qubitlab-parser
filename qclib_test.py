7b0725810e40e860f04c2855c16d5c53428bcf84
Tomasz Dworakowski <tomasz.dworakowski@clickmeeting.com>
2015-11-10 21:12:30 +0100

    Change example in qclib_test.py.

--- qclib_test.py
+++ qclib_test.py
@@ -3,6 +3,31 @@
 
 from lib.qclib.qclib import *
 
-circ = (I ** h ** I) * (I ** cnot) * (cnot2 ** I)
-print circ(ket0 ** ket0 ** ket0).dirac()
-print s2
+L = Arbitrary(s2 * array([ [ 1, -1],[1, 1],]))
+R = Arbitrary(s2 * array([ [1, 1], [-1, 1],]))
+S = Arbitrary([ [ 1j, 0], [0, 1], ])
+T = Arbitrary([ [-1, 0], [ 0, -1j], ])
+
+psi = Qubit([[2.0 / 7 * (cos(pi / 2 / 9) + 1.0j * sin(pi / 2 / 9))],
+    [sqrt(45) / 7 * (cos(pi / 3 * 2) + 1.0j * sin(pi / 3 * 2))], ])
+
+alice = (I ** L ** I) * (I ** cnot) * (cnot ** I) * (R ** I ** I)
+bob = (S ** cnot) * (I ** Swap()) * (cnot2 ** I) * \
+    (I ** Swap()) * (S ** I ** T) * (I ** Swap()) * (cnot2 ** I) * (I ** Swap())
+
+input = psi ** ket0 ** ket0
+qreg = alice(input)
+cbits = qreg.measure(1, 2)
+output = bob(qreg)
+
+print
+print "-- cbits --"
+print
+print cbits
+print
+print
+print "-- input --"
+print
+print input
+print
+print
